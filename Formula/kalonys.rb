b61d1620d27ffb2470e6a9fd6c2e2920520e6d6d
Kylian BRUN <59508914+kylianbrun@users.noreply.github.com>
2024-08-19 10:51:20 +0200

    Update kalonys to version 1.0.3

--- Formula/kalonys.rb
+++ Formula/kalonys.rb
@@ -3,7 +3,7 @@ class Kalonys < Formula
   homepage "https://kalonys.fr"
   url "https://kalonys-cli-artifacts.s3.amazonaws.com/kalonys-mac-1.0.3"
   version "1.0.3"
-  sha256 "8293bde37153891e7bbb96624de4fb2b0102c03ac4e2eec5fb504e042b12ed15"
+  sha256 "4ad1db55005ec92496b89eb0efe5895c74012460cedd2b3ffb13cf283a1775c2"
 
   def install
     bin.install "kalonys-mac-1.0.2" => "kalonys"
